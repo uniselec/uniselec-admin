56786429b39c1131b6982c0776334fe6e348e573
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-08-06 11:03:46 -0300

    Unecessary feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Login } from "./features/auth/Login";
-import { OrganizationCreate } from "./features/organizations/OrganizationCreate";
-import { OrganizationEdit } from "./features/organizations/OrganizationEdit";
-import { OrganizationList } from "./features/organizations/OrganizationList";
 import { AuthProfile } from "./features/auth/AuthProfile";
 import { NotFoundCard } from "./components/NotFoundCard";
 import { ApplicationList } from "./features/applications/ApplicationList";
@@ -26,11 +23,10 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<ProtectedRoute><UserList /></ProtectedRoute>} />
+          <Route path="/users" element={<ProtectedRoute><UserList /></ProtectedRoute>} />
 
           <Route path="/charts" element={<ProtectedRoute><ApplicationList /></ProtectedRoute>} />
-          <Route path="/organizations" element={<ProtectedRoute><OrganizationList /></ProtectedRoute>} />
-          <Route path="/organizations/edit/:id" element={<ProtectedRoute><OrganizationEdit /></ProtectedRoute>} />
-          <Route path="/organizations/create" element={<ProtectedRoute><OrganizationCreate /></ProtectedRoute>} />
+
 
           <Route path="/applications" element={<ProtectedRoute><ApplicationList /></ProtectedRoute>} />
 
