b64cf249396c0e7cea48beb3a6b043e7f99a3b62
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-08-06 10:58:34 -0300

    Improve header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,14 @@ import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Box, Button, IconButton, Toolbar } from "@mui/material";
-import { AccountMenu } from "./AccountMenu";
-import { Link } from 'react-router-dom';
-import { styled } from '@mui/material/styles';
 import LogoUNILAB from "../assets/img/logo-unilab.png";
+import { AccountMenu } from "./AccountMenu";
 
-const ImageLogo = styled('img')`
-  width: 300px;
-  padding: 30px;
-`;
+
+// const ImageLogo = styled(({ ...otherProps }) => <img alt="Logo UNILAB" src={LogoUNILAB} {...otherProps} />)`
+//   width: 300px;
+//   padding: 30px;
+// `;
 
 type HeaderProps = {
   toggle: () => void;
@@ -20,16 +19,29 @@ type HeaderProps = {
 };
 
 export function Header({ toggle, isAuth = false, isDark = false, handleDrawerToggle }: HeaderProps) {
+
   return (
     <Box>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Link to="/">
-          <ImageLogo alt="Logo UNILAB" src={LogoUNILAB}/>
-        </Link>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
         <Box sx={{ flexGrow: 1 }} />
+
         {isAuth && (
-          <AccountMenu isDark={isDark} toggleTheme={toggle} />
+          <>
+            <AccountMenu isDark={isDark} toggleTheme={toggle} />
+          </>
         )}
+        LOGO AQUI
+        {/* <ImageLogo /> */}
       </Toolbar>
     </Box>
   );
