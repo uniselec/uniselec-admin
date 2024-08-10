450e48dceb687a0c72d76ef2cda0f467fc891683
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-08-10 10:19:09 -0300

    Anotations

--- src/features/applications/DashboardApplications.tsx
+++ src/features/applications/DashboardApplications.tsx
@@ -3,6 +3,7 @@ import { Container, Grid, Paper, Typography, Card, CardContent, Box } from '@mui
 import { Link } from 'react-router-dom';
 import { Bar, Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, ArcElement, Tooltip, Legend } from 'chart.js';
+import { ApplicationCSVDownload } from './ApplicationCSVDownload';
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, ArcElement, Tooltip, Legend);
 
@@ -39,47 +40,76 @@ const DashboardApplications: React.FC = () => {
     };
 
     return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Dashboard de Resultados
-            </Typography>
+        <>
             <Card elevation={3}>
                 <CardContent>
                     <Grid container spacing={3}>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} md={4}>
+
                             <Paper elevation={3}>
                                 <Typography variant="h6" gutterBottom>
-                                    Downloads dos Arquivos Para o INEP Por Modalidade
+                                    Passo 1
                                 </Typography>
                                 <Box display="flex" flexDirection="column">
                                     <Link to="/download/modalidade-a" style={{ marginBottom: 8 }}>
-                                        Modalidade A
+                                        Exportar Inscrições
                                     </Link>
-                                    {/*
-                                    <Link to="/download/modalidade-b" style={{ marginBottom: 8 }}>
-                                        Modalidade B
-                                    </Link>
-                                    <Link to="/download/modalidade-c">
-                                        Modalidade C
-                                    </Link> */}
+
                                 </Box>
                             </Paper>
                         </Grid>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} md={4}>
+
                             <Paper elevation={3}>
                                 <Typography variant="h6" gutterBottom>
-                                    Download da Lista Preliminar de Deferidos e Indeferidos
+                                    Passo 2
+                                </Typography>
+                                <Box display="flex" flexDirection="column">
+                                    <Link to="/download/modalidade-a" style={{ marginBottom: 8 }}>
+                                        Importar Notas
+                                    </Link>
+
+                                </Box>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+
+                            <Paper elevation={3}>
+                                <Typography variant="h6" gutterBottom>
+                                    Passo 3
+                                </Typography>
+                                <Box display="flex" flexDirection="column">
+                                    <Link to="/download/modalidade-a" style={{ marginBottom: 8 }}>
+                                        Processar resultados
+                                    </Link>
+
+                                </Box>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Paper elevation={3}>
+                                <Typography variant="h6" gutterBottom>
+                                    Passo 4
+                                </Typography>
+                                <Box display="flex" flexDirection="column">
+                                    <Link to="/download/modalidade-a" style={{ marginBottom: 8 }}>
+                                        Gerar Listas
+                                    </Link>
+
+                                </Box>
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12} md={4}>
+                            <Paper elevation={3}>
+                                <Typography variant="h6" gutterBottom>
+                                    Auditoria
                                 </Typography>
                                 <Box display="flex" flexDirection="column">
                                     <Typography variant="h6" gutterBottom>
                                         Em Breve
                                     </Typography>
-                                    {/* <Link to="/download/deferidos" style={{ marginBottom: 8 }}>
-                                        Lista de Deferidos
-                                    </Link>
-                                    <Link to="/download/indeferidos">
-                                        Lista de Indeferidos
-                                    </Link> */}
+
                                 </Box>
                             </Paper>
                         </Grid>
@@ -89,7 +119,7 @@ const DashboardApplications: React.FC = () => {
             {/* <Card elevation={3} sx={{ marginBottom: 3 }}>
                 <CardContent>
                     <Grid container spacing={3}>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} md={4}>
                             <Paper elevation={3}>
                                 <Typography variant="h6" align="center" gutterBottom>
                                     Inscritos por Modalidade
@@ -97,7 +127,7 @@ const DashboardApplications: React.FC = () => {
                                 <Bar data={barData} />
                             </Paper>
                         </Grid>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} md={4}>
                             <Paper elevation={3}>
                                 <Typography variant="h6" align="center" gutterBottom>
                                     Distribuição de Inscritos
@@ -108,7 +138,7 @@ const DashboardApplications: React.FC = () => {
                     </Grid>
                 </CardContent>
             </Card> */}
-        </Container>
+        </>
     );
 };
 
