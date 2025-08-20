c5e516dc3e00296ff8d6510edc2855e81d6f32c8
Milton Cavalcante <milton.cneto@gmail.com>
2025-08-20 19:35:49 -0300

    Apply function for candidate name prioritization in DOCX list generation

--- src/features/processSelections/components/ApplicationOutcomeGenerateDocuments.tsx
+++ src/features/processSelections/components/ApplicationOutcomeGenerateDocuments.tsx
@@ -89,7 +89,11 @@ export function ApplicationOutcomeGenerateDocuments({
       classification: idx < vacancies ? "Classificado" : "Classificável",
     }));
 
-
+  // The order of the input array defines the priority
+  const getValidName = (names: Array<string | null | undefined>): string => {
+    const invalids = [null, undefined, "", "N/A"];
+    return names.find(name => !invalids.includes(name)) || "";
+  }
 
   const generateDocx = async () => {
     const doc = new Document({
@@ -169,7 +173,11 @@ export function ApplicationOutcomeGenerateDocuments({
                       new TableCell({
                         children: [
                           new Paragraph(
-                            outcome.application?.enem_score?.scores?.name || ""
+                            getValidName([
+                              outcome.application?.form_data?.social_name,    // priority 1
+                              outcome.application?.enem_score?.scores?.name, // priority 2
+                              outcome.application?.form_data?.name           // priority 3
+                            ])
                           ),
                         ],
                       }),
@@ -242,12 +250,6 @@ export function ApplicationOutcomeGenerateDocuments({
       availableWidth
     );
     doc.text(wrappedTitle, margin, margin + 70);
-
-    // The order of the input array defines the priority
-    const getValidName = (names: Array<string | null | undefined>): string => {
-      const invalids = [null, undefined, "", "N/A"];
-      return names.find(name => !invalids.includes(name)) || "";
-    }
 
     const rows = classifiedOutcomes.map((outcome, index) => [
       index + 1,
